bd5ded785a9c3f28fffcbcd3a0abcacbe524843f
Sreeraj-palat <work.sreeraj@gmail.com>
2022-07-30 12:00:29 +0530

    update selection sort

--- selectionSort.py
+++ selectionSort.py
@@ -3,15 +3,14 @@
 #sorting by finding min_index
 def selectionSort(array, size):
     
-    for ind in range(size):
-        min_index = ind
- 
-        for j in range(ind + 1, size):
+    for i in range(size):
+        
+        for j in range(i + 1, size):
             # select the minimum element in every iteration
-            if array[j] < array[min_index]:
-                min_index = j
+            if array[j] < array[i]:
+               
          # swapping the elements to sort the array
-        (array[ind], array[min_index]) = (array[min_index], array[ind])
+             array[i], array[j] = array[j], array[i]
  
 arr = [-2, 45, 0, 11, -9,88,-97,-202,747]
 size = len(arr)
